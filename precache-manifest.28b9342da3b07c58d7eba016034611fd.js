bee46cd29dd4f980f225162a4b0c131f49b70bf4
Allin Ihor <ui.ethuil@gmail.com>
2019-02-09 23:02:36 +0200

    Updates

---
+++ precache-manifest.28b9342da3b07c58d7eba016034611fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e15dea6b53b046c2d545",
+    "url": "/calculator-reactjs/static/js/runtime~main.e15dea6b.js"
+  },
+  {
+    "revision": "2c8410d4497149335887",
+    "url": "/calculator-reactjs/static/js/main.2c8410d4.chunk.js"
+  },
+  {
+    "revision": "f225114d8151a0afe232",
+    "url": "/calculator-reactjs/static/js/1.f225114d.chunk.js"
+  },
+  {
+    "revision": "2c8410d4497149335887",
+    "url": "/calculator-reactjs/static/css/main.2d75997f.chunk.css"
+  },
+  {
+    "revision": "f225114d8151a0afe232",
+    "url": "/calculator-reactjs/static/css/1.4f46d4f2.chunk.css"
+  },
+  {
+    "revision": "c5bc2e2a2078f5204a32e0234883c4f1",
+    "url": "/calculator-reactjs/index.html"
+  }
+];
